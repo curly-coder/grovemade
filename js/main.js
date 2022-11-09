5dd78d4425f1818918f47c41b027e4dcfa137d38
Anastasiia Kumeda <78300119+curly-coder@users.noreply.github.com>
2022-11-09 19:09:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-// $('.hamburger').on('click', function(){
-//     $('.menu').toggle();
-//   })
+$(".owl-carousel").owlCarousel({
+    items: 4,
+    dots: true, 
+    loop:true,
+    responsive : {
+        0:{
+            items:1,
+            dots:false
+        }, 
+        485 :{
+            items:2, 
+            dots:true
+        }, 
+        728 :{
+            items: 3,
+            dots:true
+        }, 
+        960 :{
+            items: 3,
+            dots:true
+        }, 
+        1200 : {
+            items: 4,
+            dots: true
+        }
+    }
+});
+
+
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 
@@ -8,3 +34,4 @@ menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
